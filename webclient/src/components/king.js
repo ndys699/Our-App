6dba5163711bc3a750aa41dbc31da23e088c755c
shankarsantoshgorle <shankarsantosh.gorle88@gmail.com>
2022-11-05 15:22:34 +0530

    added

--- webclient/src/components/king.js
+++ webclient/src/components/king.js
@@ -8,6 +8,7 @@ function king() {
       <h1>This is Mahiraavana</h1>
       <h1>This is E-commerce website</h1>
       <h1>Satish anna laptop got some problem</h1>
+      <h1>We are going to have a party in the evening with sandeep and satish</h1>
       </div>
     
 
