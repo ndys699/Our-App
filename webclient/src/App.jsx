625f9e28686a02119a9bd81e48c8309b76277e99
sandeep <ndys699@gmail.com>
2022-11-05 17:42:57 +0530

    added some

---
+++ webclient/src/App.jsx
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+
+class NavBar extends Component{
+render(){
+    return (
+    <React.Fragment>
+        <h1>Hello</h1>
+        <p>This is Paragraph</p>
+    </React.Fragment>
+    );
+}
+}
+
+
+export default NavBar;
