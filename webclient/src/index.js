b1652d97515b9abc7d2b9a87b518e5e12825584b
Shaik Masrad <masradsk9471@gmail.com>
2022-11-07 13:19:33 +0530

    added something

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -4,12 +4,12 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import NavBar  from "./NavBar";
+// import NavBar  from "./NavBar";
 import "./index.css";
 import App from "./App";
 
 
 
-ReactDOM.render(<NavBar></NavBar>,document.getElementById("root"));
+ReactDOM.render(<App></App>,document.getElementById("root"));
 
 
